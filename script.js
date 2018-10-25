88311c2bd3d0c489e64ff49b3eabc1a8d473a4d9
Thomas Perez <thomasperez@uagateway.org>
2018-10-25 18:39:49 +0000

    Centered many things and got remove button to work

--- script.js
+++ script.js
@@ -1,19 +1,23 @@
 $(document).ready(function() {
 	// Varialbes Used for Logic Go Below:
-
 	// Your Code Goes Here!
 	$("#add-button").click(function() {
-		$("ol").append("<li>" + $("#list-input").val() + "<button class='remove-button'>" + "Delete" + "</button>" + "<button class='edit-button onclick='edit()'>" + "Edit" + "</button>" + "</li>");
+		$("ol").append("<li>" + $("#list-input").val() + "<button class=\"remove-button\" onclick=\"remove()\">" + "Delete" + "</button>" + "<button class=\"edit-button\" onclick=\"edit()\">" + "Edit" + "</button>" + "</li>");
 	});
 	$("#delete-button").click(function() {
 		$("li").remove();
 	});
-
-
+	
 });
-
 function edit() {
 	debugger;
 	$(this).parent().append("<input class='edit-input'>");
 	console.log($(this).parent());
+}
+
+function remove() {
+	$(".remove-button").click(function() {
+		console.log("removing");
+		$(this).parent().remove();
+	});
 }
